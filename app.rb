58651205ed5a690d114e9e09c80f1bb267f22872
Enis <enis.mema@gmail.com>
2023-05-23 13:18:59 +0200

    Fix save_data.rb

--- app.rb
+++ app.rb
@@ -8,7 +8,6 @@ require_relative 'modules/mod_book'
 require_relative 'modules/mod_people'
 require_relative 'modules/mod_rentals'
 require_relative 'read_data'
-require_relative 'save_data'
 
 class App
   attr_accessor :books, :rentals, :people
@@ -21,12 +20,6 @@ class App
     @books = ReadData.new.read_books
     @rentals = []
     @people = []
-  end
-
-  def quit_app
-  SaveData.new.save_books(@books)
-  puts 'Thank you for using this app! Now exiting...'
-  exit
   end
 
 end
--- save_data.rb
+++ save_data.rb
@@ -2,16 +2,20 @@ require 'json'
 require 'fileutils'
 
 class SaveData
-  def self.create_file(path, array)
+def self.create_file(path, array)
     FileUtils.mkdir_p('./data')
     FileUtils.touch(path) unless File.exist?(path)
     File.write(path, JSON.pretty_generate(array))
   end
+def save_books(books)
+books_array = []
+books.each do |book|
+    books_array << {title: book.title, author: book.author}
+end
+return if books_array.empty?
+check_file_exist('books')
+File.write('./data/books.json', JSON.pretty_generate(books_array))
+end
 
-  def self.save_books(books)
-    books_array = books.map { |book| { title: book.title, author: book.author } }
-    return if books_array.empty?
 
-    create_file('./data/books.json', books_array)
-  end
 end
